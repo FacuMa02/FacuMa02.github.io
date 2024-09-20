5dd17dde98ae9a294db554d0b7efe26a70656e5f
unknown <facumaidana2001@gmail.com>
2024-09-20 10:24:02 -0300

    Generar link directo para la instalacion de la pwa

--- js/index.js
+++ js/index.js
@@ -1,3 +1,23 @@
+let deferredPrompt;
+
+window.addEventListener("beforeinstallprompt", (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+  // Muestra el botón de instalación aquí
+});
+
+const installApp = () => {
+  deferredPrompt.prompt();
+  deferredPrompt.userChoice.then((choiceResult) => {
+    if (choiceResult.outcome === "accepted") {
+      console.log("User accepted the install prompt");
+    } else {
+      console.log("User dismissed the install prompt");
+    }
+    deferredPrompt = null;
+  });
+};
+
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
